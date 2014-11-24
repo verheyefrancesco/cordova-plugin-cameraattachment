d6a009fe36159d16c08ee56dda430c93ff780630
Francesco Verheye <francesco.verheye@bitmunks.com>
2014-11-24 11:51:45 +0100

    gen added

---
+++ src/android/gen/com/checkroom/plugin/cameraattachment/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.checkroom.plugin.cameraattachment;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
